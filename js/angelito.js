b9aba0e6387d02c52dda546b1804db9658ad5b46
dfeagans <dane.feagans@gmail.com>
2014-10-05 18:06:06 +0000

    Plan added to README.md

---
+++ js/angelito.js
@@ -0,0 +1,33 @@
+var config = {
+    content: [{
+	type: 'row',
+	content:[{
+	    type: 'component',
+	    componentName: 'testComponent',
+	    componentState: { label: 'A' }
+	},{
+	    type: 'column',
+	    content:[{
+		type: 'component',
+		componentName: 'testComponent',
+		componentState: { label : 'B' }
+	    },{
+		type: 'component',
+		componentName: 'testComponent',
+		componentState: { label : 'C' }
+	    }]
+	}]
+    }]
+};
+
+var myLayout = new GoldenLayout( config );
+
+myLayout.registerComponent( 'testComponent', function( container, componentState ){
+    container.getElement().html( '<h2>' + componentState.label + '<h2>'); 
+});
+
+myLayout.init();
+
+myLayout.on( 'stateChanged', function(){
+    alert('Test');
+});
